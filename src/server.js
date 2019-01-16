be32f483858d05ada3266af41ffa1b55f345e087
build-failure <michael@lewkowski.de>
2019-01-16 10:00:25 +0100

    Add basic auth protected json endpoint

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const app = express();
 
+const basicAuth = require('basic-auth');
+
 const multer  = require('multer')
 const uploadDir='/usr/var/app/upload'
 const storage = multer.diskStorage({
@@ -17,6 +19,18 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage: storage })
 
+const auth = function (req, res, next) {
+  var user = basicAuth(req);
+  if (!user || !user.name || !user.pass || !(user.name === 'admin' && user.pass === 'secret')) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    res.sendStatus(401);
+  } else {
+    next();
+  }
+}
+
+app.all("/admin/*", auth);
+
 app.use(morgan('combined'));
 
 const serve = serveStatic(path.join(__dirname, '../public'), { 'index': ['index.html', 'index.htm'] });
@@ -28,6 +42,17 @@ app.get('/healthcheck',function (req, res) {
 
 app.post('/upload/single', upload.single('file'), function (req, res, next) {
   res.send('OK');
+});
+
+
+app.get('/foo', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send({'foo': 'bar'});
+});
+
+app.get('/admin/foo', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send({'foo': 'baz'});
 });
 
 let port = 3000;
